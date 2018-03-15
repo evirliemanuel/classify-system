3c518e4e30b293f86070a1b243190c8be731b869
gglifekillmetot <lieverandiver@gmail.com>
2018-03-15 21:37:59 +0800

    Update MainActivity2, remove the tablayout student

--- classify-android/app/src/main/java/com/lieverandiver/thesisproject/MainActivity2.java
+++ classify-android/app/src/main/java/com/lieverandiver/thesisproject/MainActivity2.java
@@ -104,21 +104,16 @@ public class MainActivity2 extends AppCompatActivity implements ClassAdapter.Cla
         tabLayout.getTabAt(1).setCustomView(tabTwo);
 
         TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
-        tabThree.setText("Student");
+        tabThree.setText("Setting");
         tabThree.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.small_home, 0, 0);
         tabLayout.getTabAt(2).setCustomView(tabThree);
 
-        TextView tabFour = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
-        tabFour.setText("Setting");
-        tabFour.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.small_home, 0, 0);
-        tabLayout.getTabAt(3).setCustomView(tabFour);
     }
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new SliderScheduleFragment(), "Shedule");
         adapter.addFragment(new SliderClassFragment(), "Class");
-        adapter.addFragment(new Home_Student_Slidebar_Fragment(), "Student");
         adapter.addFragment(new SliderSettingFragment(), "Setting");
         viewPager.setAdapter(adapter);
     }
